bbb28f0de31fba5713f74e96c0446b71c8e84a66
Madu Chiemelie Precious <101350287+Maduemelie@users.noreply.github.com>
2023-06-29 22:47:04 +0100

    hosting

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,11 +1,15 @@
 const redis = require("redis");
-
+require('dotenv').config()
 // Create a Redis client
-const redisClient = redis.createClient({
-  host: "127.0.0.1",
-  port: 6379,
-});
+const password = process.env.REDIS_PASSWORD;
 
+const redisClient = redis.createClient({
+  password: password,
+  socket: {
+      host: 'redis-10922.c11.us-east-1-3.ec2.cloud.redislabs.com',
+      port: 10922
+  }
+});
 // Handle Redis client connection errors
 redisClient.on("error", (error) => {
   console.error("Redis connection error:", error);
@@ -14,16 +18,6 @@ redisClient.on("error", (error) => {
 // Wait for Redis client to connect
 redisClient.on("connect", () => {
   console.log("Connected to Redis");
-
-  // // Use the Redis client to execute commands here
-  // // ...
-  // redisClient.set("name", "john", (error, result) => {
-  //   if (error) {
-  //     console.error("Error setting Redis key:", error);
-  //   } else {
-  //     console.log("Redis key set successfully");
-  //   }
-  // });
 });
 
 module.exports = redisClient;
