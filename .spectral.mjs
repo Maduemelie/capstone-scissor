617b087fcd8d0ac892216c637b90171c01a49c1b
Madu Chiemelie Precious <maduemelie@gmail.com>
2023-06-30 14:21:42 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODY5NQ/spectral.js?branch_slug=main&token=580ec524-c547-46f5-85fa-b3c58143c21b";
+export default { extends: ruleset };
